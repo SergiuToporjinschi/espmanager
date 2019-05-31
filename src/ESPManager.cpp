7948317f862c917559edfa28370212871d074655
sergiu_toporjinschi <sergiu.toporjinschi@gmail.com>
2019-05-31 16:26:49 +0300

    add md5 scheck to getInfo

--- src/ESPManager.cpp
+++ src/ESPManager.cpp
@@ -450,13 +450,13 @@ void ESPManager::cmdGetInfo(const char *respTopic, JsonVariant params) {
   String coreVersion = ESP.getCoreVersion();
   coreVersion.replace("_", ".");
 
-  char skVerBuf[30] = {0};
+  char skVerBuf[90] = {0};
   if (sketchVersion != nullptr) {
-    snprintf_P(skVerBuf, 30, SKETCH_VERSION_PATTERN_P, sketchVersion);
+    snprintf_P(skVerBuf, 90, SKETCH_VERSION_PATTERN_P, sketchVersion, ESP.getSketchMD5().c_str());
   }
 
-  char retVal[510] = {0};
-  snprintf_P(retVal, 510, INFO_PATTERN_P, WiFi.hostname().c_str(), ESP.getChipId(), WiFi.localIP().toString().c_str(), String(WiFi.macAddress()).c_str(), WiFi.RSSI(), ESP.getResetReason().c_str(), ESP.getFlashChipId(), coreVersion.c_str(),
+  char retVal[600] = {0};
+  snprintf_P(retVal, 600, INFO_PATTERN_P, WiFi.hostname().c_str(), ESP.getChipId(), WiFi.localIP().toString().c_str(), String(WiFi.macAddress()).c_str(), WiFi.RSSI(), ESP.getResetReason().c_str(), ESP.getFlashChipId(), coreVersion.c_str(),
              ESP.getSdkVersion(), ESP.getVcc() / 1024.00f, ESP.getFlashChipSpeed() / 1000000, ESP.getCycleCount(), ESP.getCpuFreqMHz(), ESP.getFreeHeap(), ESP.getFlashChipSize(), ESP.getSketchSize(),
              ESP.getFreeSketchSpace(), ESP.getFlashChipRealSize(), version, skVerBuf);
 
--- src/ESPManager.h
+++ src/ESPManager.h
@@ -37,7 +37,7 @@ static const char STATUS_OFFLINE_P[] PROGMEM = "offline";
 static const char UPDATE_SKETCH_P[] PROGMEM = "sketch";
 static const char UPDATE_SPIFFS_P[] PROGMEM = "spiffs";
 
-static const char SKETCH_VERSION_PATTERN_P[] = ",\"sketchVersion\":\"%s\"";
+static const char SKETCH_VERSION_PATTERN_P[] = ",\"sketchVersion\":\"%s\",\"sketchMD5\":\"%s\" ";
 static const char INFO_PATTERN_P[] PROGMEM = "{\"hostName\":\"%s\",\"chipId\":%i,\"localIP\":\"%s\",\"macAddress\":\"%s\",\"RSSI\":\"%i\",\"lastRestartReson\":\"%s\",\"flashChipId\":%u,\"coreVersion\":\"%s\",\"sdkVersion\":\"%s\",\"vcc\":\"%1.2f V\",\"flashChipSpeed\":\"%u MHz\",\"cycleCount\":%u,\"cpuFreq\":\"%u MHz\", \"freeHeap\":%u,\"flashChipSize\":%u,\"sketchSize\":%u,\"freeSketchSpace\":%u,\"flashChipRealSize\":%u,\"espManagerVersion\":\"%s\"%s}";
 
 enum ESPManConnStatus {
